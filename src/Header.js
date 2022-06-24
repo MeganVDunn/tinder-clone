6930fbc78d9635a22257052d4c8c101cda8e2673
Megan <iamdun24@gmail.com>
2022-06-24 09:08:18 -0400

    icons finally

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Header.css';
+import PersonIcon from '@mui/icons-material/Person';
 
 function Header() {
   return (
     <div className='header'>
         <h2>I am a header</h2>
+        <PersonIcon  fontSize='large' className='header-icon' />
     </div>
   )
 }
