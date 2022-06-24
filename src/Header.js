cc6a5591ac25dae2c4ea00fe377479a74ca85c45
Megan <iamdun24@gmail.com>
2022-06-24 09:43:44 -0400

    header css

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import './Header.css';
 import PersonIcon from '@mui/icons-material/Person';
+import IconButton from '@mui/material/IconButton';
 
 function Header() {
   return (
     <div className='header'>
-        <h2>I am a header</h2>
-        <PersonIcon  fontSize='large' className='header-icon' />
+        
+        <IconButton>
+            <PersonIcon  fontSize='large' className='header_icon' />
+        </IconButton>
+
+        <img
+            className='header_logo' 
+            src="https://www.citypng.com/public/uploads/preview/-11595270396ei6vchptvb.png"
+            alt=""
+        />
+        
     </div>
   )
 }
